6f9af61af5d9a7a1261424d44b50b4db00c539a9
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-23 04:43:32 +0530

    eleven

--- index.js
+++ index.js
@@ -3,7 +3,30 @@ var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
+
+//yaha se
 var db = require('./db.json');
+
+function isAuthenticated({username, password}){
+  return userdb.users.findIndex(user => user.username === username && user.password === password) !== -1
+}
+
+server.post('/users/login', (req, res) => {
+  console.log("login endpoint called; request body:");
+  console.log(req.body);
+  const {username, password} = req.body;
+  if (isAuthenticated({username, password}) === false) {
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: false, status: 'Login Unsuccessful', err: 401});
+  }
+  //const access_token = createToken({username, password})
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'Login Successful', token: "7iuh78yh" });
+})
+//yaha tak
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
