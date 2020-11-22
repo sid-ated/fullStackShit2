147f27c3c3785208d980bdaefb72a27384d4e6b6
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-23 04:39:40 +0530

    ten

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
+var db = require('./db.json');
 server.use(middlewares);
 server.use(router);
 server.listen(port, function () {
