2285bd76a9abd89b962e6a8a12ad7b825537695b
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 04:27:39 +0530

    something added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
+var serverAuth = require('./serverAuth');
+server.use('/auth', serverAuth);
 server.use(router);
 server.listen(port, function () {
     console.log('JSON Server is running')
