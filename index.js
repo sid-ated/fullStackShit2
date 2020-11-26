d38bcd0ca85e17f0795014131b91bf07523055f2
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 13:57:50 +0530

    auth removed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
 var serverAuth = require('./serverAuth');
-server.use('/auth', serverAuth);
+//server.use('/auth', serverAuth);
 server.use(router);
 server.listen(port, function () {
     console.log('JSON Server is running')
