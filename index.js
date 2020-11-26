4da812de4817d0f71b30b0c5edbf2ec4751f23ea
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 14:02:44 +0530

    sample

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
 //var serverAuth = require('./serverAuth');
 //server.use('/auth', serverAuth);
+server.get('/auth', (req, res) => {
+res.status(200).jsonp("I AM WOORKING")
+})
 server.use(router);
 server.listen(port, function () {
     console.log('JSON Server is running')
