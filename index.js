a84f949d47acaad9ff191c21be105aecc1300e14
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 14:10:54 +0530

    okay

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 var port = Number(process.env.PORT || 3000);
 server.use(middlewares);
-//var serverAuth = require('./serverAuth');
-//server.use('/auth', serverAuth);
+/*
+var serverAuth = require('./serverAuth');
+server.use('/auth', serverAuth);
 server.get('/auth', (req, res) => {
 res.status(200).jsonp("I AM WOORKING")
 })
+*/
 server.use(router);
 server.listen(port, function () {
     console.log('JSON Server is running')
