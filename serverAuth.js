2d5be621c795ef22d42cec7766029bcbbce453f3
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 13:52:56 +0530

    login

--- serverAuth.js
+++ serverAuth.js
@@ -23,35 +23,28 @@ function verifyToken(token){
   return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
 }
 
-async function getapi(url) { 
-    
-    // Storing response 
-    const response = await fetch(url); 
-    
-    // Storing data in form of JSON 
-    var data = await response.json(); 
-    //console.log(data); 
-    
-    return data;
-} 
-
 serverAuth.get('/temp', (req, res) => {
-let url = "https://aichemist-server.herokuapp.com/users";
+//const url = "http://localhost:3000/users";
+const url = "https://aichemist-server.herokuapp.com/users";
 fetch(url)
 .then((resp) => resp.json()) 
 .then(function(data) {
   console.log(data)
 })
+.catch(err => console.log(err))
 res.status(200).jsonp("working");
 });
 
 // Check if the user exists in database
 function isAuthenticated({username, password}){
-fetch("https://aichemist-server.herokuapp.com/users")
-.then((resp) => resp.json()) 
-.then(function(data) {
-  return data.findIndex(user => user.username === username && user.password === password) !== -1
-})
+	//const url = "http://localhost:3000/users";
+	const url = "https://aichemist-server.herokuapp.com/users";
+	fetch(fetch)
+	.then((resp) => resp.json()) 
+	.then(function(data) {
+	  return data.findIndex(user => user.username === username && user.password === password) !== -1
+	})
+	.catch(err => console.log(err))
 }
 
 // Register New User
@@ -59,19 +52,21 @@ serverAuth.post('/register', (req, res) => {
   console.log("register endpoint called; request body:");
   console.log(req.body);
   const {username, password} = req.body;
-  var okay = isAuthenticated({username, password});
+  
   if(isAuthenticated({username, password}) === true) {
     res.statusCode = 401;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: false, status: 'This user already exist', err: 401});
   }
-else{
-	fetch("https://aichemist-server.herokuapp.com/users")
+	  
+	const url = "https://aichemist-server.herokuapp.com/users";
+	//let url = "http://localhost:3000/users";
+	fetch(url)
 	.then((resp) => resp.json()) 
 	.then(function(data) {
 	  var last_item_id = data[data.length-1].id;
 	  //data.push({id: last_item_id + 1, username: username, password: password, isAdmin: false});
-		    fetch("https://aichemist-server.herokuapp.com/users", { 
+		    fetch(url, { 
 		      
 		    // Adding method type 
 		    method: "POST", 
@@ -91,17 +86,17 @@ else{
 		    } 
 		}) 
 		.then(response => response.json()) 
-  
-		// Displaying results to console 
-		.then(json => console.log(json)); 
-	})
 
-// Create token for new user
-  const access_token = createToken({username, password})
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'Registration Successful! You are loged in.', token: access_token });
-  }
+		// Displaying results to console 
+		.then(json => console.log(json));
+	})
+	.catch(err => console.log(err))
+
+	// Create token for new user
+	  const access_token = createToken({username, password})
+	  res.statusCode = 200;
+	  res.setHeader('Content-Type', 'application/json');
+	  res.json({success: true, status: 'Registration Successful! You are loged in.', token: access_token });
 })
 
 // Login to one of the users from ./users.json
@@ -114,12 +109,11 @@ serverAuth.post('/login', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.json({success: false, status: 'Login Unsuccessful', err: 401});
   }
-  else{
-	  const access_token = createToken({username, password})
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'application/json');
-	  res.json({success: true, status: 'Login Successful', token: access_token });
-  }
+  const access_token = createToken({username, password})
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'Login Successful', token: access_token });
+
 })
 
 serverAuth.use(/^(?!\/auth).*$/,  (req, res, next) => {
